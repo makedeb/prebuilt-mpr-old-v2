c849d9b83470a6d3588504952796b4d01062865e
Hunter Wittenborn <hunter@hunterwittenborn.com>
2022-05-14 15:34:39 -0500

    Updated template files

--- .drone/scripts/build.sh
+++ .drone/scripts/build.sh
@@ -9,6 +9,8 @@ pkgdir="/var/tmp/prebuilt-mpr/${pkgname}/${distro_codename}"
 # Install needed packages.
 sudo apt update
 sudo apt install jq git gpg curl lsb-release -y
+sudo apt-get install wget sudo apt-utils ca-certificates apt-transport-https -y
+sudo apt-get install lsb-release gettext wget curl build-essential cmake g++ sudo gpg git autoconf automake libtool pkg-config libpthread-stubs0-dev libssl-dev libevent-pthreads-2.1-7 zstd -y
 
 # Add Prebuilt-MPR repository to system so we can access packages from there during builds.
 if [[ "${exclude_pbmpr:-x}" == 'x' ]]; then
@@ -23,10 +25,13 @@ sudo chown 'makedeb:makedeb' ./ /var/tmp/prebuilt-mpr/ -R
 # Destroy all files in pkgdir.
 find "${pkgdir}" -mindepth 1 -maxdepth 1 -exec rm -rf '{}' +
 
-# Build the package.
+# Copy files to the packaging directory and build the package.
+# We pass '--pass-env' so that the DEBIAN_FRONTEND variable (which is set in
+# the Drone CI environment automatically in our instance) is exposed to
+# 'sudo apt-get' calls.
 cd pkg/
 find ./ -mindepth 1 -maxdepth 1 -exec cp '{}' "${pkgdir}/{}" -R \;
 cd "${pkgdir}"
-makedeb -s --no-confirm --skip-pgp-check
+makedeb -s --no-confirm --skip-pgp-check --pass-env
 
 # vim: set sw=4 expandtab:
